2616c299d1f0b74b5a889bcc2a8b185d28966385
anasany <anasanyspm@gmail.com>
2024-12-23 23:16:11 +0100

    anass changed something

--- src/main/java/com/example/web_appp/Main.java
+++ src/main/java/com/example/web_appp/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         database db = new database();
         db.add_operation(10, 200, 30);
-       // from ubuntu
-        //
+        // anass changed this
     }
 }
