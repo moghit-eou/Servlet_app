516ea68e8811a2e3afbef46a1d5622d4428a2b91
moghit-eou <moghit.elouaad1@gmail.com>
2024-12-23 21:59:02 +0100

    trying to understand git stuff

--- src/main/java/com/example/web_appp/Main.java
+++ src/main/java/com/example/web_appp/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         database db = new database();
         db.add_operation(10, 200, 30);
-        // edited from github 
+
+        // deleted and edited from intellij again
     }
 }
